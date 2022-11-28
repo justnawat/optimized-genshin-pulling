2370688dd909f011502c41165331eb7ba4be48c4
nawat <n.ngerncham@gmail.com>
2022-11-28 10:26:32 +0700

    cleaner added for both data set

--- data/clean02.py
+++ data/clean02.py
@@ -3,7 +3,10 @@
 import os
 import sys
 import pandas as pd
-import json
+import random as rd
+
+
+SAMPLE_SIZE = 200
 
 
 def generate_default_df():
@@ -17,34 +20,35 @@ def generate_default_df():
 
 
 if len(sys.argv) != 2:
-    print("Usage ./clean01.py <base/path/to/data/>")
+    print("Usage ./clean02.py <base/path/to/data/>")
 
 base_path = sys.argv[1]
-sub_dirs = filter(lambda s: s.isnumeric(), os.listdir(f"{base_path}/01/"))
+sub_dirs = rd.sample(os.listdir(f"{base_path}/02/"), SAMPLE_SIZE)
 
 translation = {
-    "抽卡时间": "DateTime",
-    "名称": "Name",
-    "类别": "Type",
-    "星级": "Rarity",
-    "武器": "Weapon",
-    "角色": "Character"
+    "name": "Name",
+    "gacha_type": "Banner",
+    "item_type": "Type",
+    "rank_type": "Rarity",
+    "gacha_id": "Pull ID",
+    "gacha_time": "DateTime",
+    0: "Character",
+    1: "Weapon"
 }
 
 
 combined_df = generate_default_df()
 
 for sub_dir in sub_dirs:
-    def load_df(banner: int) -> pd.DataFrame:
-        current_file_path = f"{base_path}/01/{sub_dir}/gacha{banner}.csv"
-        if os.path.exists(current_file_path) and os.path.getsize(current_file_path) != 0:
+    def load_df() -> pd.DataFrame:
+        current_file_path = f"{base_path}/02/{sub_dir}"
+        if os.path.getsize(current_file_path) != 0:
             print(f"reading: {current_file_path}")
             data = pd\
                 .read_csv(current_file_path)\
                 .rename(columns=translation)
             data["Type"] = data["Type"].replace(translation)
-            data = data.drop(["Name"], axis=1)
-            data["Banner"] = banner
+            data = data.drop(["Name", "Pull ID"], axis=1)
 
             pairs = data["DateTime"].map(lambda s: s.split())
             data["Date"] = [pair[0] for pair in pairs]
@@ -54,14 +58,10 @@ for sub_dir in sub_dirs:
             return data
         return generate_default_df()
 
-    permanent_df = load_df(200)
-    character_banner_df = load_df(301)
-    weapon_banner_df = load_df(302)
-
-    combined_df = pd.concat(
-        [combined_df, permanent_df, character_banner_df, weapon_banner_df])
+    new_df = load_df()
+    combined_df = pd.concat([combined_df, new_df])
 
 combined_df\
     .reindex(columns=["Rarity", "Date", "Time", "Banner", "Type"])\
     .astype({"Banner": int, "Rarity": int})\
-    .to_csv(f"{base_path}/data01.csv", index=False)
+    .to_csv(f"{base_path}/data02.csv", index=False)
